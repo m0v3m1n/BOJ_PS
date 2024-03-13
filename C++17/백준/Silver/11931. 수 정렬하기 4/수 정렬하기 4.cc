340ae37851b50230a64fcd222abd02b08db5159c
m0v3m1n <126737225+m0v3m1n@users.noreply.github.com>
2024-03-13 10:12:03 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 672 ms, Memory: 5928 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int N; std::cin >> N;
+
+	int* arr = new int[N];
+
+	for (int i = 0; i < N; i++)
+		std::cin >> arr[i];
+
+	std::sort(arr, arr + N, std::greater<int>());
+
+	for (int i = 0; i < N; i++)
+		std::cout << arr[i] << '\n';
+
+	delete[] arr;
+
+	return 0;
+}
