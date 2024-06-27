146ca74c83bd8794734b451939825012a33bcc5d
m0v3m1n <126737225+m0v3m1n@users.noreply.github.com>
2024-06-27 22:35:46 +0900

    [Silver IV] Title: 좋은 단어, Time: 20 ms, Memory: 2268 KB -BaekjoonHub

---
+++ C++17/백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+
+int main()
+{
+	int T; std::cin >> T;
+	int count = 0;
+	char C;
+
+	while (T--)
+	{
+		std::string S; std::cin >> S;
+		std::stack<char> ST;
+
+		for (int i = 0; i < S.size(); i++)
+		{
+			if (ST.empty())
+				ST.push(S[i]);
+			else
+			{
+				C = S[i];
+
+				if (ST.top() == C)
+					ST.pop();
+				else
+					ST.push(C);
+			}
+		}
+
+		if (ST.empty())
+			count++;
+	}
+	
+	std::cout << count;
+	return 0;
+}
