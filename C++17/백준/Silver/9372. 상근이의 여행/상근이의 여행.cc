2efbb0730f624b0943ce5c18e80c4cd8df09573e
m0v3m1n <126737225+m0v3m1n@users.noreply.github.com>
2024-07-04 22:04:35 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ C++17/백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 int main()
 {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
 	int T, N, M, cA, cB;
 	std::cin >> T;
 
@@ -11,35 +13,10 @@ int main()
 	{
 		std::cin >> N >> M;
 
-		bool* visited = new bool[N + 1];
-		std::fill(visited, visited + N + 1, false);
-		// 동적 배열 선언.
-
-		int count = 0;
-
-		std::vector<std::vector<int>> V(N + 1); // 2차원 Vector
 		for (int i = 0; i < M; i++)
-		{
-			std::cin >> cA >> cB; // 나라 2개 입력받기.
-			V[cA].push_back(cB);
-			V[cB].push_back(cA); // 쌍방으로 넣어줌.
-		}
+			std::cin >> cA >> cB;
 
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 0; j < V[i].size(); j++)
-			{
-				if (!visited[V[i][j]]) // i번째 나라 출발하는 비행기 중, j번째 나라로 가는 비행기 안 탔으면
-				{
-					count++;
-					visited[V[i][j]] = true; // 방문 처리 후 count 추가.
-				}
-			}
-		}
-		std::cout << count - 1 << '\n';
-
-		// 동적 배열 삭제
-		delete[] visited;
+		std::cout << N - 1 << '\n'; // 결국 (정점의 개수 - 1)번만 타면 됨.
 	}
 	return 0;
 }
