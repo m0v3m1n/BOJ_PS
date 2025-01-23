384b4d9d46572eeb661533a78a140686c87c02c6
m0v3m1n <126737225+m0v3m1n@users.noreply.github.com>
2025-01-23 23:59:35 +0900

    [Silver V] Title: 금메달, 은메달, 동메달은 누가?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/3230. 금메달， 은메달， 동메달은 누가？/금메달， 은메달， 동메달은 누가？.cc
+++ C++17/백준/Silver/3230. 금메달， 은메달， 동메달은 누가？/금메달， 은메달， 동메달은 누가？.cc
@@ -1,46 +1,25 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-vector<int> v1; // 1차 순서
-vector<int> v2; // 최종 순서
+int main()
+{
+	int N, M, tmp; std::cin >> N >> M;
+	std::vector<int> fv, sv;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> tmp;
+		fv.insert(fv.begin() + (tmp - 1), i);
+	}
 
-    int n, m;
-    cin >> n >> m;
+	for (int i = 0; i < M; i++)
+	{
+		std::cin >> tmp;
+		sv.insert(sv.begin() + (tmp - 1), fv[M - 1 - i]);
+	}
 
-    // 1차 순서를 입력받아 v1에 저장
-    for (int i = 1; i <= n; i++) {
-        int x;
-        cin >> x;
-        v1.insert(v1.begin() + (x - 1), i);
-    }
+	for (int i = 0; i < 3; i++)
+		std::cout << sv[i] + 1 << '\n';
 
-    // v1에서 m명까지 남기기
-    while (v1.size() > m) {
-        v1.pop_back();
-    }
-
-    // 2차 순서에 따라 v2에 최종 순서 정리
-    for (int i = 1; i <= m; i++) {
-        int x;
-        cin >> x;
-        v2.insert(v2.begin() + (x - 1), v1.back());
-        v1.pop_back();
-    }
-
-    // v2에서 상위 3명만 남기기
-    while (v2.size() > 3) {
-        v2.pop_back();
-    }
-
-    // 결과 출력
-    for (auto it = v2.begin(); it != v2.end(); it++) {
-        cout << *it << '\n';
-    }
-
-    return 0;
+	return 0;
 }
