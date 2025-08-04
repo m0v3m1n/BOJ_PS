a4c2161864a50aac994581190e4ec97a7c0a8a8f
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2025-08-04 10:45:11 +0900

    [Silver IV] Title: Maximum Subarray, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/10211. Maximum Subarray/Maximum Subarray.cc
+++ C++17/백준/Silver/10211. Maximum Subarray/Maximum Subarray.cc
@@ -10,27 +10,22 @@ int main()
 
 	while (T--)
 	{
-		int N, max = 1000 * -1000 - 1; // 1,000개 * 절댓값 1000, 최댓값보다 1 더 작게끔 --> -1.
-		std::cin >> N;
+		int N; std::cin >> N;
 		int* arr = new int[N];
 
 		std::cin >> arr[0];
 		for (int i = 1; i < N; i++)
-		{
 			std::cin >> arr[i];
-			arr[i] += arr[i - 1];
-		}
-		
-		// 처음부터 i까지 더했을 때 가장 큰 경우
-		for (int i = 0; i < N; i++)
-			if (max < arr[i])
-				max = arr[i];
 
-		// 0부터 N까지 = i & 0부터 N - 1까지 = j ==> arr[i] - arr[j] ==> 누적합의 부분배열
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < i; j++)
-				if (max < arr[i] - arr[j])
-					max = arr[i] - arr[j];
+		int max = arr[0];
+		int cur = arr[0];
+
+		for (int i = 1; i < N; i++)
+		{
+			// 카데인 알고리즘
+			cur = arr[i] > cur + arr[i] ? arr[i] : cur + arr[i];
+			max = max > cur ? max : cur;
+		}
 
 		std::cout << max << '\n';
 		delete[] arr;
