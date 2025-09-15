b5a437458c70954c7113f6b154fbd3fe5b08b80b
이동민 <126737225+m0v3m1n@users.noreply.github.com>
2025-09-15 16:35:12 +0900

    [Bronze III] Title: 완전 세제곱, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/4690. 완전 세제곱/완전 세제곱.cc
+++ C++17/백준/Bronze/4690. 완전 세제곱/완전 세제곱.cc
@@ -7,11 +7,31 @@ int main()
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
+    int arr[101];
+    for (int i = 0; i <= 100; i++)
+        arr[i] = i * i * i;
+
     for (int a = 2; a <= 100; a++)
+    {
+        int a_3 = arr[a];
+
         for (int b = 2; b < a; b++)
+        {
+            int b_3 = arr[b];
+
             for (int c = b; c < a; c++)
+            {
+                int c_3 = arr[c];
+
                 for (int d = c; d < a; d++)
-                    if (std::pow(a, 3) == std::pow(b, 3) + std::pow(c, 3) + std::pow(d, 3))
+                {
+                    int d_3 = arr[d];
+
+                    if(a_3 == b_3 + c_3 + d_3)
                         std::cout << "Cube = " << a << ", Triple = (" << b << "," << c << "," << d << ")" << '\n';
+                }
+            }
+        }
+    }
     return 0;
 }
